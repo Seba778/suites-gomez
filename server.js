03ee2cd8000e8321b72b04661260585b5a9234c0
Sebastian Schamne <sebastianschamne1@gmail.com>
2026-01-14 13:03:34 -0300

    Fix CORS headers

--- server.js
+++ server.js
@@ -31,8 +31,10 @@ const SuiteSchema = new mongoose.Schema({
 const Suite = mongoose.model('Suite', SuiteSchema);
 
 app.use(cors({
-  origin: "https://suites-gomez-1f4y51apt-sebastian-schamnes-projects.vercel.app",
-  credentials: true
+  origin: ["https://suites-gomez-1f4y51apt-sebastian-schamnes-projects.vercel.app", "http://localhost:5173"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // --- WEBHOOK DE STRIPE (Debe ir antes de express.json) ---
